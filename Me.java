35b15b1834fdd8f6bab29cc9776ed374c0b7e8b4
gaelle tohme <gaelletohme2004@gmail.com>
2023-01-25 20:57:48 +0200

    My year of study added

--- Me.java
+++ Me.java
@@ -2,5 +2,6 @@ public class Me{
 	public  static void main(String[] args){
 		System.out.println("Gaelle Tohme");
 		System.out.println("Computer engineering");
+		System.out.println("1st year of study");
 	}
 }
