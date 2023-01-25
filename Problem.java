02de0063a47b2a06d4c026eb6805bac158f94346
gaelle tohme <gaelletohme2004@gmail.com>
2023-01-25 20:53:14 +0200

    the problem has been corrected

--- Problem.java
+++ Problem.java
@@ -1,6 +1,6 @@
-public Static Class Problems{
+public static class Problem{
 	public static main (String[] args){
-		system.out.print("Hello,I have a lot of ptoblems here");
+		System.out.print("Hello,I have a lot of ptoblems here");
 		System.out.println("Can You HELP me in that plz?");
 	}
 }
